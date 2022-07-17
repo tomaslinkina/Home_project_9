42c8996c680d411d262df85977aa59364d1978a4
tomaslinkina <altoma@mail.ru>
2022-07-17 16:22:54 +0500

    Initial commit

---
+++ src/main/java/Post.java
@@ -0,0 +1,13 @@
+import java.util.Date;
+
+public class Post {
+    public String name;
+    public String surname;
+    public String passport;
+    public String patronymic;
+    public FormDate birthday;
+    public String phone;
+    public boolean subscription;
+
+
+}
